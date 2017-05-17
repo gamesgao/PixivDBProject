8e34d19803c8b3fb4834930ae2c7e1cc20ec4a82
chyacinth <hyacinth@hyacinthdeMacBook-Pro.local>
2017-05-17 22:14:41 +0800

    调试调试

--- dbconf/pool.js
+++ dbconf/pool.js
@@ -1,4 +1,10 @@
-var dbparam = require('../dbconf/db.js');
 var mysql = require('mysql');
-var pool = mysql.createPool(dbparam);
+
+//创建连接池
+var pool  = mysql.createPool({
+    host     : '127.0.0.1',
+    user     : 'root',
+    password : '',
+    database : 'dbproject'
+});
 module.exports = pool;
