9788ac6dcae6d7626f5f77c2b56d10dd3d01f66a
liuwuliuyun <liuwuliuyun@qq.com>
2017-05-27 12:22:04 +0800

    初步完成数据库

--- dbconf/sqlMapping_Filling.js
+++ dbconf/sqlMapping_Filling.js
@@ -62,7 +62,7 @@ var user = {
     queryById: 'select * from user where id=?;',
     queryAll: 'select * from user;',
     cancelTrade :'CALL cancelTrade(?,?);',//userID tradeID
-    searchUserByName :'select * from user where username LIKE ?;',
+    searchUserByName :'SELECT * FROM user WHERE username like ?;',
     modifyUserPassword :'SELECT modifyUserPassword(?,?,?);',//第一个参数是oldUserPassword，第二个参数是newUserPassword，第三个参数是userID
     modifyUserBasicInfo :'UPDATE user SET username = ?, alipay_address = ? WHERE id = ?;'
 };
