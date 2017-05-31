59b62838991ca3c9269798e69ac6506bb401a16a
顾尧 <gamesgao@163.com>
2017-05-31 23:31:06 +0800

    添加twitter

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var options = {
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '',
+    password: 'f1403018409',
     database: 'dbproject'
 };
 
--- dbconf/pool.js
+++ dbconf/pool.js
@@ -1,12 +1,12 @@
 var mysql = require('mysql');
 
 //创建连接池
-var pool  = mysql.createPool({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'dbproject',
-    port : 3306,
-    multipleStatements : true
+var pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'f1403018409',
+    database: 'dbproject',
+    port: 3306,
+    multipleStatements: true
 });
 module.exports = pool;
